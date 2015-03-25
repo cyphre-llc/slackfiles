be4e192df3e7cc8798a4d86bb072e9701ac6d994
Ben Collins <ben.c@servergy.com>
2015-03-25 13:40:37 -0400

    SVY: Take advantage of autoconfig type
    Eliminates the need to click around to activate SlackFiles share.
    
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- appinfo/app.php
+++ appinfo/app.php
@@ -4,7 +4,7 @@
  * Copyright (c) 2015 Servergy, Inc.
  */
 
-if (\OCP\App::isEnabled('files_external')) {
+if (\OCP\App::isEnabled('files_external') and \OCP\App::isEnabled('slacknotify')) {
 	$l = \OC_L10N::get('slackfiles');
 
 	OC::$CLASSPATH['OC\Files\Storage\Slack'] = 'slackfiles/lib/slack.php';
@@ -12,6 +12,9 @@ if (\OCP\App::isEnabled('files_external')) {
 	OC_Mount_Config::registerBackend('\OC\Files\Storage\Slack', array(
 		'backend' => 'Slack Files',
 		'priority' => 100,
-		'configuration' => array( 'justme' => '!'.$l->t('Only my files')),
+		'configuration' => array(
+			'justme' => '!'.$l->t('Only my files'),
+			'auto' => '>',
+		),
 		'has_dependencies' => true));
 }
